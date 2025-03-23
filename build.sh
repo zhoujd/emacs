5a9cb8f3f0bade9e7382ebb2cc5df6bb7411969b
Zachary Zhou <zchrzhou@gmail.com>
2025-03-23 17:59:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,32 +3,50 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 BUILD_ROOT=$SCRIPT_ROOT/src/build
 
+. /etc/os-release
+
 EMACS_VER=27.2
 EMACS_SRC=emacs-${EMACS_VER}
 EMACS_PREFIX=/usr/local/${EMACS_SRC}
 EMACS_OPT="--with-modules"
 
 dep() {
-    sudo apt update
-    sudo apt install -y automake
-    sudo apt install -y build-essential
-    sudo apt install -y libxft-dev libotf-dev libgpm-dev imagemagick
-    sudo apt install -y libxpm-dev libpng-dev libjpeg-dev libtiff-dev libgif-dev
-    sudo apt install -y libxaw7-dev libncurses5-dev libgtk2.0-dev librsvg2-dev libgconf2-dev
-    sudo apt install -y libm17n-dev libgnutls28-dev libselinux1-dev libdbus-1-dev
+    case $ID in
+        ubuntu|debian )
+            echo "Install on $ID"
+            sudo apt update
+            sudo apt install -y automake
+            sudo apt install -y build-essential
+            sudo apt install -y libxft-dev libotf-dev libgpm-dev imagemagick
+            sudo apt install -y libxpm-dev libpng-dev libjpeg-dev libtiff-dev libgif-dev
+            sudo apt install -y libxaw7-dev libncurses5-dev libgtk2.0-dev librsvg2-dev libgconf2-dev
+            sudo apt install -y libm17n-dev libgnutls28-dev libselinux1-dev libdbus-1-dev
+            ;;
+        * )
+            echo "Unsupport $ID"
+            ;;
+    esac
     echo "Install build deps done"
 }
 
 tool() {
-    sudo apt install -y cscope
-    sudo apt install -y emacs-bin-common  # etags
-    sudo apt install -y texinfo
-    sudo apt install -y markdown pandoc
-    sudo apt install -y w3m
-    sudo apt install -y silversearcher-ag ripgrep
-    sudo apt install -y socat
-    sudo apt install -y perl-doc
-    sudo apt install -y ccls
+    case $ID in
+        ubuntu|debian )
+            echo "Install on $ID"
+            sudo apt install -y cscope
+            sudo apt install -y emacs-bin-common  # etags
+            sudo apt install -y texinfo
+            sudo apt install -y markdown pandoc
+            sudo apt install -y w3m
+            sudo apt install -y silversearcher-ag ripgrep
+            sudo apt install -y socat
+            sudo apt install -y perl-doc
+            sudo apt install -y ccls
+            ;;
+        * )
+            echo "Unsupport $ID"
+            ;;
+    esac
     echo "Install emacs tools done"
 }
 
